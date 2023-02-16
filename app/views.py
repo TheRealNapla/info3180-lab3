3b60eecee0aced65104a119a9fbfc7a255c9cc49
Peter Thelwell <pthelwell55@gmail.com>
2023-02-16 16:57:18 -0500

    Began working on the form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from forms import ContactForm
 
 
 ###
@@ -17,6 +18,10 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route("/contact", methods = ["GET", "POST"])
+def contact():
+    form = ContactForm()
+    return render_template("contact.html", form = form)
 
 ###
 # The functions below should be applicable to all Flask apps.
